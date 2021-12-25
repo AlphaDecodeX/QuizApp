07e7f7027f06b892b8d716b71a3faa018ef0385a
unknown <lpsk5713@gmail.com>
2021-12-25 15:14:33 +0530

    Stateless vs Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,19 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
   void answerQuestion() {
-    print("Answer Choosen");
+    questionIndex += 1;
   }
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What is your favorite color?',
+      'What is your favorite animal'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -20,9 +27,10 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text("The Question"),
+            Text(questions[questionIndex]),
             RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
-            RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion),
+            RaisedButton(
+                child: Text('Answer 2'), onPressed: () => {print("Answer 2")}),
             RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion),
           ],
         ),
