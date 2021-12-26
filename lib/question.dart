18fe1493c223770fcbc46e9616a7151d958ca2b1
unknown <lpsk5713@gmail.com>
2021-12-26 20:04:14 +0530

    Layout with Objects and Custom Widgets

--- lib/question.dart
+++ lib/question.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  String questionText;
+  final String questionText; //Will not be able to change the Text
 
   Question(this.questionText);
   // First Argument == questionText (Positional Argument not a named one)
   // {First Argument} == named argument.
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+        width: double.infinity,
+        margin: EdgeInsets.all(10),
+        child: Text(
+          questionText,
+          style: TextStyle(fontSize: 28),
+          textAlign: TextAlign.center,
+        ));
   }
 }
